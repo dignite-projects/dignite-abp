6577913e07835dc25b37234c4f090799e3545c5e
duguankui <hello@dignite.com>
2022-09-14 18:39:13 +0800

    optimize aspnet mvc pure theme

--- modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/Toolbars/PureThemeMainTopToolbarContributor.cs
+++ modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/Toolbars/PureThemeMainTopToolbarContributor.cs
@@ -1,39 +1,30 @@
 ﻿using System.Threading.Tasks;
-using Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure.Themes.Pure.Components.Toolbar.LanguageSwitch;
 using Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure.Themes.Pure.Components.Toolbar.UserMenu;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Toolbars;
-using Volo.Abp.Localization;
 using Volo.Abp.Users;
 
 namespace Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure.Toolbars;
 
 public class PureThemeMainTopToolbarContributor : IToolbarContributor
 {
-    public async Task ConfigureToolbarAsync(IToolbarConfigurationContext context)
+    public Task ConfigureToolbarAsync(IToolbarConfigurationContext context)
     {
         if (context.Toolbar.Name != StandardToolbars.Main)
         {
-            return;
+            return Task.CompletedTask;
         }
 
         if (!(context.Theme is PureTheme))
         {
-            return;
+            return Task.CompletedTask;
         }
 
-        var languageProvider = context.ServiceProvider.GetService<ILanguageProvider>();
-
-        //TODO: This duplicates GetLanguages() usage. Can we eleminate this?
-        var languages = await languageProvider.GetLanguagesAsync();
-        if (languages.Count > 1)
-        {
-            context.Toolbar.Items.Add(new ToolbarItem(typeof(LanguageSwitchViewComponent)));
-        }
 
         if (context.ServiceProvider.GetRequiredService<ICurrentUser>().IsAuthenticated)
         {
             context.Toolbar.Items.Add(new ToolbarItem(typeof(UserMenuViewComponent)));
         }
+        return Task.CompletedTask;
     }
 }

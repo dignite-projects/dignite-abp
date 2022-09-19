76401879fc2de0bd1122c524081fc53b67bca82c
duguankui <hello@dignite.com>
2022-09-19 09:54:08 +0800

    Update FakeNotificationDefinitionManager.cs

--- framework/test/Dignite.Abp.Notifications.Tests/Dignite/Abp/Notifications/FakeNotificationDefinitionManager.cs
+++ framework/test/Dignite.Abp.Notifications.Tests/Dignite/Abp/Notifications/FakeNotificationDefinitionManager.cs
@@ -4,26 +4,25 @@ using Microsoft.Extensions.Options;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Features;
 
-namespace Dignite.Abp.Notifications.Tests.Dignite.Abp.Notifications
-{
-    public class FakeNotificationDefinitionManager : NotificationDefinitionManager, ISingletonDependency
-    {
-        public FakeNotificationDefinitionManager(
-            IOptions<NotificationOptions> options,
-            IServiceProvider serviceProvider,
-            IFeatureChecker featureChecker
-        ) : base(
-                options,
-                serviceProvider,
-                featureChecker
-                )
-        {
-        }
+namespace Dignite.Abp.Notifications;
 
-        protected override Task<bool> PermissionCheckAsync(NotificationDefinition notificationDefinition, Guid userId)
-        {
-            return Task.FromResult( true);
-        }
+public class FakeNotificationDefinitionManager : NotificationDefinitionManager, ISingletonDependency
+{
+    public FakeNotificationDefinitionManager(
+        IOptions<NotificationOptions> options,
+        IServiceProvider serviceProvider,
+        IFeatureChecker featureChecker
+    ) : base(
+            options,
+            serviceProvider,
+            featureChecker
+            )
+    {
+    }
+
+    protected override Task<bool> PermissionCheckAsync(NotificationDefinition notificationDefinition, Guid userId)
+    {
+        return Task.FromResult( true);
     }
 }
 

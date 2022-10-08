bd4617f01cdc33de3a3db1bc0f47a87263c4698a
duguankui <hello@dignite.com>
2022-10-08 14:23:43 +0800

    Update ConsoleTestAppHostedService.cs

--- modules/notification-center/test/Dignite.Abp.NotificationCenter.HttpApi.Client.ConsoleTestApp/ConsoleTestAppHostedService.cs
+++ modules/notification-center/test/Dignite.Abp.NotificationCenter.HttpApi.Client.ConsoleTestApp/ConsoleTestAppHostedService.cs
@@ -5,34 +5,33 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Volo.Abp;
 
-namespace Dignite.Abp.NotificationCenter
+namespace Dignite.Abp.NotificationCenter;
+
+public class ConsoleTestAppHostedService : IHostedService
 {
-    public class ConsoleTestAppHostedService : IHostedService
+    private readonly IConfiguration _configuration;
+
+    public ConsoleTestAppHostedService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-
-        public ConsoleTestAppHostedService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            using (var application = AbpApplicationFactory.Create<AbpNotificationCenterConsoleApiClientModule>(options =>
-            {
-                options.Services.ReplaceConfiguration(_configuration);
-                options.UseAutofac();
-            }))
-            {
-                application.Initialize();
-
-                var demo = application.ServiceProvider.GetRequiredService<ClientDemoService>();
-                await demo.RunAsync();
-
-                application.Shutdown();
-            }
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+        _configuration = configuration;
     }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        using (var application = AbpApplicationFactory.Create<AbpNotificationCenterConsoleApiClientModule>(options =>
+        {
+            options.Services.ReplaceConfiguration(_configuration);
+            options.UseAutofac();
+        }))
+        {
+            application.Initialize();
+
+            var demo = application.ServiceProvider.GetRequiredService<ClientDemoService>();
+            await demo.RunAsync();
+
+            application.Shutdown();
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 }

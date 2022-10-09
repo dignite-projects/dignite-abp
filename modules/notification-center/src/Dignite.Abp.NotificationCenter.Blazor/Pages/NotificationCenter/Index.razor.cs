c75741208fa1a5768d1633499cbec8e88765dd5f
duguankui <hello@dignite.com>
2022-10-09 18:43:54 +0800

    Create Index.razor.cs

---
+++ modules/notification-center/src/Dignite.Abp.NotificationCenter.Blazor/Pages/NotificationCenter/Index.razor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dignite.Abp.NotificationCenter.Blazor.Pages.NotificationCenter;
+public partial class Index
+{
+}

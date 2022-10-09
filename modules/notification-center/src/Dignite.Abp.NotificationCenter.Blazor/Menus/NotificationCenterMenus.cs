7899f9c63acd295f557ef4bf3c989815eea296be
duguankui <hello@dignite.com>
2022-10-09 18:43:16 +0800

    remove

--- modules/notification-center/src/Dignite.Abp.NotificationCenter.Blazor/Menus/NotificationCenterMenus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dignite.Abp.NotificationCenter.Blazor.Menus;
-
-public class NotificationCenterMenus
-{
-    public const string Prefix = "NotificationCenter";
-
-    //Add your menu items here...
-    //public const string Home = Prefix + ".MyNewMenuItem";
-}

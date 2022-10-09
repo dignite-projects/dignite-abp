2500ddafab4a66e744170c81a1fe46ba3bdb2e77
杜冠魁 <hello@dignite.com>
2022-10-09 23:40:09 +0800

    Update Index.razor.cs

--- modules/notification-center/src/Dignite.Abp.NotificationCenter.Blazor/Pages/NotificationCenter/Index.razor.cs
+++ modules/notification-center/src/Dignite.Abp.NotificationCenter.Blazor/Pages/NotificationCenter/Index.razor.cs
@@ -3,8 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dignite.Abp.Notifications;
+using Microsoft.AspNetCore.SignalR.Client;
+using Volo.Abp.Application.Dtos;
 
 namespace Dignite.Abp.NotificationCenter.Blazor.Pages.NotificationCenter;
 public partial class Index
 {
+    /// <summary>
+    /// notification total count
+    /// </summary>
+    private int totalCount = 0;
+    private int skipCount = 0;
+    private int maxResultCount = 30;
+
+    private List<UserNotificationDto> userNotifications = null;
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        totalCount = await NotificationAppService.GetCountAsync(null);
+        userNotifications = await GetListAsync();
+        await base.OnInitializedAsync();
+    }
+
+    private async Task LoadMoreAsync()
+    {
+        skipCount += maxResultCount;
+        userNotifications.AddRange(await GetListAsync());
+    }
+
+    async Task<List<UserNotificationDto>> GetListAsync()
+    {
+        return (await NotificationAppService.GetListAsync(null, skipCount, maxResultCount))
+            .Items
+            .ToList();
+    }
 }

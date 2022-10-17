87835cd155a66e36a2a3824076549fe237a90aea
杜冠魁 <hello@dignite.com>
2022-10-17 22:36:18 +0800

    Create DirectoryDescriptorEto.cs

---
+++ modules/file-explorer/src/Dignite.FileExplorer.Domain.Shared/Dignite/FileExplorer/Directories/DirectoryDescriptorEto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Dignite.FileExplorer.Directories
+{
+    public class DirectoryDescriptorEto
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Container name of blob
+        /// </summary>
+        public string ContainerName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid? ParentId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Order { get; set; }
+
+        public Guid? TenantId { get; set; }
+
+    }
+}
+

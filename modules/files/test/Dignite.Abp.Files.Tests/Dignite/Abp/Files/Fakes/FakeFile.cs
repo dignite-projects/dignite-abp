a757d14e2cdb3d4237dc42d6b0dbd90e19bbf5b9
duguankui <hello@dignite.com>
2022-11-03 21:33:37 +0800

    test

--- modules/files/test/Dignite.Abp.Files.Tests/Dignite/Abp/Files/Fakes/FakeFile.cs
+++ modules/files/test/Dignite.Abp.Files.Tests/Dignite/Abp/Files/Fakes/FakeFile.cs
@@ -2,39 +2,15 @@
 using Volo.Abp.DependencyInjection;
 
 namespace Dignite.Abp.Files.Fakes;
-public class FakeFile : IFile, ITransientDependency
-{
 
-    public FakeFile(string containerName, string blobName, long size, string name, string mimeType)
+public class FakeFile : FileBase, ITransientDependency
+{
+    public FakeFile(Guid id, string containerName, string blobName, string name, string mimeType)
+        : base(id, containerName, blobName, name, mimeType)
     {
         ContainerName = containerName;
         BlobName = blobName;
-        Size = size;
         Name = name;
         MimeType = mimeType;
-    }
-
-    public string ContainerName { get; set; }
-
-    public string BlobName { get; set; }
-
-    public long Size { get; set; }
-
-    public string Name { get; set; }
-
-    public string MimeType { get; set; }
-
-    public Guid Id { get; set; }
-
-    public Guid? TenantId { get; set; }
-
-    public object[] GetKeys()
-    {
-        return new object[] { Id };
-    }
-
-    public void Resize(long size)
-    {
-        this.Size = size;   
     }
 }
--- modules/files/test/Dignite.Abp.Files.Tests/Dignite/Abp/Files/Fakes/FakeFileStore.cs
+++ modules/files/test/Dignite.Abp.Files.Tests/Dignite/Abp/Files/Fakes/FakeFileStore.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Volo.Abp.DependencyInjection;
 
 namespace Dignite.Abp.Files.Fakes;
+
 public class FakeFileStore : IFileStore<FakeFile>, ITransientDependency
 {
-    public Task<bool> BlobNameExistsAsync(string containerName, string blobName, Guid? ignoredId = null, CancellationToken cancellationToken = default)
+    public Task<bool> BlobNameExistsAsync(string containerName, string blobName, CancellationToken cancellationToken = default)
     {
         return Task.FromResult(false);
     }
@@ -21,8 +21,23 @@ public class FakeFileStore : IFileStore<FakeFile>, ITransientDependency
         return Task.CompletedTask;
     }
 
-    public Task<FakeFile> FindAsync(string containerName, string blobName, CancellationToken cancellationToken = default)
+    public Task<FakeFile> FindByBlobNameAsync(string containerName, string blobName, CancellationToken cancellationToken = default)
     {
         return Task.FromResult<FakeFile>(null);
+    }
+
+    public Task<FakeFile> FindByMd5Async(string containerName, string md5, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult<FakeFile>(null);
+    }
+
+    public Task<bool> Md5ExistsAsync(string containerName, string md5, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(false);
+    }
+
+    public Task<bool> ReferencingAnyAsync(string containerName, string blobName, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(false);
     }
 }

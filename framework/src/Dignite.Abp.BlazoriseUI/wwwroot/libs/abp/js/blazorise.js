016b0fcf2e8ac50508eb8ff6a4eddc0209e8cf0d
duguankui <hello@dignite.com>
2022-11-10 16:01:33 +0800

    Update blazorise.js

--- framework/src/Dignite.Abp.BlazoriseUI/wwwroot/libs/abp/js/blazorise.js
+++ framework/src/Dignite.Abp.BlazoriseUI/wwwroot/libs/abp/js/blazorise.js
@@ -19,33 +19,15 @@
             }
         }
 
-        let fixedHeaderTable = document.getElementById(id);
-        // The distance from the table content to the top
-        let fixedHeaderTableTop = 0
-        if (fixedHeaderTable) {
-            fixedHeaderTableTop = fixedHeaderTable.getBoundingClientRect().top
-        }
-        else {
-            console.warn("table-fixed-header is not found.Set the initial value of scrolly in the ant table component.");
-            return 500;
-        }
-
-        // Form height - The height of the top of the table content - The height of the bottom of the table content
-        let height = `calc(100vh - ${fixedHeaderTableTop + extraHeight}px)`;
-        return height;
+        return window.blazoriseUi.calculateFullScreenElementHeight(id, extraHeight);
     },
 
-    calculateHeight: function (id, extraHeight) {
-        if (typeof extraHeight == "undefined") {
-            //Default bottom pagination 50 +  Margin 10
-            extraHeight = 70;
-        }
-
-        let fixedHeaderTable = document.getElementById(id);
+    calculateFullScreenElementHeight: function (id, extraHeight) {
+        let element = document.getElementById(id);
         // The distance from the table content to the top
-        let fixedHeaderTableTop = 0
-        if (fixedHeaderTable) {
-            fixedHeaderTableTop = fixedHeaderTable.getBoundingClientRect().top
+        let topMarginHeight = 0
+        if (element) {
+            topMarginHeight = element.getBoundingClientRect().top
         }
         else {
             console.warn("table-fixed-header is not found.Set the initial value of scrolly in the ant table component.");
@@ -53,7 +35,7 @@
         }
 
         // Form height - The height of the top of the table content - The height of the bottom of the table content
-        let height = `calc(100vh - ${fixedHeaderTableTop + extraHeight}px)`;
+        let height = `calc(100vh - ${topMarginHeight + extraHeight}px)`;
         return height;
     }
 };

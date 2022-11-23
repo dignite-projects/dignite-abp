d2c38a7103550cdc42c7dc19587e187484aed718
duguankui <hello@dignite.com>
2022-11-23 16:14:15 +0800

    test: add file form sample

---
+++ samples/FileExplorerSample/FileExplorerSample/Pages/FileForm.razor.cs
@@ -0,0 +1,24 @@
+
+
+using Dignite.FileExplorer.Blazor.Pages.FileExplorer;
+using Dignite.FileExplorer.Files;
+
+namespace FileExplorerSample.Pages;
+
+public partial class FileForm
+{
+    string EntityId = "6526e1c4-deeb-3110-f979-3a07b0bb8d49";
+    List<FileDescriptorDto> Files = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Files = (await FileDescriptorAppService.GetListAsync(new GetFilesInput
+        {
+            ContainerName = Services.SampleContainer.SampleContainerName,
+            SkipCount = 0,
+            MaxResultCount = 100,
+            EntityId = EntityId
+        })).Items.ToList();
+        await base.OnInitializedAsync();
+    }
+}
--- samples/FileExplorerSample/FileExplorerSample/Pages/Index.razor.cs
+++ samples/FileExplorerSample/FileExplorerSample/Pages/Index.razor.cs
@@ -2,5 +2,5 @@
 
 public partial class Index
 {
-
+    
 }

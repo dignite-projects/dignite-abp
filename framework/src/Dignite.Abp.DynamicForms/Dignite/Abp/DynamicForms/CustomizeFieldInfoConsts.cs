0dda03d6836929e5a388fce2f55784c1cb8c1fd5
杜冠魁 <hello@dignite.com>
2023-03-25 15:11:11 +0900

    Update CustomizeFieldInfoConsts.cs

--- framework/src/Dignite.Abp.DynamicForms/Dignite/Abp/DynamicForms/CustomizeFieldInfoConsts.cs
+++ framework/src/Dignite.Abp.DynamicForms/Dignite/Abp/DynamicForms/CustomizeFieldInfoConsts.cs
@@ -6,22 +6,22 @@ public static class CustomizeFieldInfoConsts
     /// Maximum length of the <see cref="ICustomizeFieldInfo.Name"/> property.
     /// Default value: 64
     /// </summary>
-    public const int MaxNameLength = 64;
+    public static int MaxNameLength { get; set; } = 64;
 
     /// <summary>
     /// Regular Expression of the <see cref="ICustomizeFieldInfo.Name"/> property.
     /// </summary>
-    public const string NameRegularExpression = "^[a-zA-Z][A-Za-z0-9_]+$";
+    public const string NameRegularExpression = "^[a-zA-Z][A-Za-z0-9_-]+$";
 
     /// <summary>
     /// Maximum length of the <see cref="ICustomizeFieldInfo.DisplayName"/> property.
     /// Default value: 64
     /// </summary>
-    public const int MaxDisplayNameLength = 64;
+    public static int MaxDisplayNameLength { get; set; } = 64;
 
     /// <summary>
     /// Maximum length of the <see cref="ICustomizeFieldInfo.FormName"/> property.
     /// Default value: 128
     /// </summary>
-    public const int MaxFormNameLength = 64;
+    public static int MaxFormNameLength { get; set; } = 64;
 }

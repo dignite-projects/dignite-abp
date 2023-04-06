ac1483cf990626211fb0a5cd6f87bd3998bc91fa
杜冠魁 <hello@dignite.com>
2023-04-06 10:52:43 +0900

    feat: add page footer

---
+++ modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/Themes/Pure/Components/PageFooter/PageFooterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure.Themes.Pure.Components.PageFooter;
+
+public class PageFooterViewComponent : AbpViewComponent
+{
+    public virtual IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}

61e4505449d7cd8aedec1673e8c1b52e67b51efa
杜冠魁 <hello@dignite.com>
2023-04-07 11:06:18 +0900

    Update TableColumnFieldDefinition.cs

--- framework/src/Dignite.Abp.DynamicForms/Dignite/Abp/DynamicForms/Table/TableColumnFieldDefinition.cs
+++ framework/src/Dignite.Abp.DynamicForms/Dignite/Abp/DynamicForms/Table/TableColumnFieldDefinition.cs
@@ -4,46 +4,15 @@ using JetBrains.Annotations;
 namespace Dignite.Abp.DynamicForms.Table;
 
 [Serializable]
-public class TableColumnCustomField : ICustomizeFieldInfo
+public class TableColumnCustomField : CustomizeFieldBase
 {
-    public TableColumnCustomField()
-    {
-        this.FormConfiguration = new FormConfigurationDictionary();
-    }
-
+    public TableColumnCustomField() : base() { }
     public TableColumnCustomField(
         [NotNull] string name,
         [NotNull] string displayName,
         [NotNull] string fieldName,
         [NotNull] string defaultValue,
         [NotNull] FormConfigurationDictionary configuration
-        )
-    {
-        Name = name;
-        DisplayName = displayName;
-        FormName = fieldName;
-        DefaultValue = defaultValue;
-        FormConfiguration = configuration;
-    }
+        ) : base(name, displayName, fieldName, defaultValue, configuration) { }
 
-    [NotNull]
-    public string Name { get; set; }
-
-    [NotNull]
-    public string DisplayName { get; set; }
-
-    /// <summary>
-    /// The form to be used to <see cref="IForm.Name"/>
-    /// </summary>
-    [NotNull]
-    public string FormName { get; set; }
-
-    /// <summary>
-    /// Default value of the field.
-    /// </summary>
-    [CanBeNull]
-    public string DefaultValue { get; set; }
-
-    [NotNull]
-    public FormConfigurationDictionary FormConfiguration { get; set; }
 }

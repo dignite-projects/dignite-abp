16a2010ee04a7de924b46c308c5222a4e9a27c72
杜冠魁 <hello@dignite.com>
2023-07-03 22:22:23 +0900

    Update FileExplorerBlazorServerModule.cs

--- modules/file-explorer/src/Dignite.FileExplorer.Blazor.Server/FileExplorerBlazorServerModule.cs
+++ modules/file-explorer/src/Dignite.FileExplorer.Blazor.Server/FileExplorerBlazorServerModule.cs
@@ -1,4 +1,5 @@
-﻿using Volo.Abp.AspNetCore.Components.Server.Theming;
+﻿using Microsoft.AspNetCore.SignalR;
+using Volo.Abp.AspNetCore.Components.Server.Theming;
 using Volo.Abp.Modularity;
 
 namespace Dignite.FileExplorer.Blazor.Server;
@@ -9,5 +10,14 @@ namespace Dignite.FileExplorer.Blazor.Server;
     )]
 public class FileExplorerBlazorServerModule : AbpModule
 {
-
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        // Error uploading file in Blazor server mode: "Did not receive any data in the allocated time"
+        // Please refer to the following link for details
+        // https://github.com/dotnet/aspnetcore/issues/38842#issuecomment-1342540950
+        Configure<HubOptions>(options =>
+        {
+            options.DisableImplicitFromServicesParameters = true;
+        });
+    }
 }

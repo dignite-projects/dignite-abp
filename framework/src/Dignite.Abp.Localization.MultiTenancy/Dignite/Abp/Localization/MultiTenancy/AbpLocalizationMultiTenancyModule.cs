d1ff706b9a2b465f427d96f48d894feebed55dab
杜冠魁 <hello@dignite.com>
2023-07-28 11:04:23 +0900

    Introducing the Volo.Abp.MultiTenancy.dll package
    在 Abp7.3版本中，Volo.Abp.Settings.dll 取消了Volo.Abp.MultiTenancy.dll的引用，因此，在本项目中手动引入Volo.Abp.MultiTenancy.dll

--- framework/src/Dignite.Abp.Localization.MultiTenancy/Dignite/Abp/Localization/MultiTenancy/AbpLocalizationMultiTenancyModule.cs
+++ framework/src/Dignite.Abp.Localization.MultiTenancy/Dignite/Abp/Localization/MultiTenancy/AbpLocalizationMultiTenancyModule.cs
@@ -1,10 +1,12 @@
 ﻿using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.MultiTenancy;
 
 namespace Dignite.Abp.Localization.MultiTenancy;
 
 [DependsOn(
-    typeof(AbpLocalizationModule)
+    typeof(AbpLocalizationModule),
+    typeof(AbpMultiTenancyModule)
     )]
 public class AbpLocalizationMultiTenancyModule : AbpModule
 {

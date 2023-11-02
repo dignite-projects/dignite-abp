0a4db626f78e0b13918e8259ad671fd5c57635bd
杜冠魁 <hello@dignite.com>
2023-11-02 10:06:51 +0900

    Update DignitePointsBlockOptions.cs

--- modules/user-points/src/Dignite.Abp.UserPoints.Domain/Dignite/Abp/UserPoints/DignitePointsBlockOptions.cs
+++ modules/user-points/src/Dignite.Abp.UserPoints.Domain/Dignite/Abp/UserPoints/DignitePointsBlockOptions.cs
@@ -4,8 +4,11 @@ namespace Dignite.Abp.UserPoints;
 public class DignitePointsBlockOptions
 {
     /// <summary>
-    /// Points Redemption Factor,i.e. how much money can be redeemed for $1.
+    /// Points value conversion Factor, usually can be understood as how many points can be exchanged for 1 yuan of currency.
     /// Default value: 1;
     /// </summary>
+    /// <remarks>
+    /// The unit value of user points must be a multiple of the Factor, for example: the Factor is 100, the number of user points can be a multiple of 100\200\300 and so on.
+    /// </remarks>
     public int Factor { get; set; } = 1;
 }

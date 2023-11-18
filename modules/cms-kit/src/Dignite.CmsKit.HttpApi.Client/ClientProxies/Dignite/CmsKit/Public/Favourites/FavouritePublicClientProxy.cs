ed2a98a99fab11b6db69c6b212657487f455f0e8
杜冠魁 <hello@dignite.com>
2023-11-18 22:22:25 +0900

    chore: Generate proxy code

---
+++ modules/cms-kit/src/Dignite.CmsKit.HttpApi.Client/ClientProxies/Dignite/CmsKit/Public/Favourites/FavouritePublicClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of FavouritePublicClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace Dignite.CmsKit.Public.Favourites;
+
+public partial class FavouritePublicClientProxy
+{
+}
---
+++ modules/cms-kit/src/Dignite.CmsKit.HttpApi.Client/ClientProxies/Dignite/CmsKit/Public/Visits/VisitPublicClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of VisitPublicClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace Dignite.CmsKit.Public.Visits;
+
+public partial class VisitPublicClientProxy
+{
+}

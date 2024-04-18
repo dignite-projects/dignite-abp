4e766df217486daae47dd1fb182c27befbeb0c7d
杜冠魁 <hello@dignite.com>
2024-04-18 22:20:07 +0900

    Improve PureTheme

--- modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/Themes/Pure/Components/BottomNavbar/BottomNavbarViewComponent.cs
+++ modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/Themes/Pure/Components/BottomNavbar/BottomNavbarViewComponent.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Toolbars;
 
 namespace Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure.Components.BottomNavbar;
 
 public class BottomNavbarViewComponent : AbpViewComponent
 {
-    public virtual IViewComponentResult Invoke()
+    protected IToolbarManager ToolbarManager { get; }
+
+    public BottomNavbarViewComponent(IToolbarManager toolbarManager)
     {
-        return View();
+        ToolbarManager = toolbarManager;
+    }
+
+    public virtual async Task<IViewComponentResult> InvokeAsync()
+    {
+        var toolbar = await ToolbarManager.GetAsync(PureToolbars.BottomNavigationBar);
+        return View(toolbar);
     }
 }
--- modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/Themes/Pure/Components/PageFooter/PageFooterViewComponent.cs
+++ modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/Themes/Pure/Components/PageFooter/PageFooterViewComponent.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.UI.Navigation;
 
 namespace Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure.Components.PageFooter;
 
 public class PageFooterViewComponent : AbpViewComponent
 {
-    public virtual IViewComponentResult Invoke()
+    private readonly IMenuManager _menuManager;
+
+    public PageFooterViewComponent(IMenuManager menuManager)
     {
-        return View();
+        _menuManager = menuManager;
+    }
+
+
+
+    public virtual async Task<IViewComponentResult> InvokeAsync()
+    {
+        var menu = await _menuManager.GetAsync(PureMenus.SiteMap);
+        return View(menu);
     }
 }

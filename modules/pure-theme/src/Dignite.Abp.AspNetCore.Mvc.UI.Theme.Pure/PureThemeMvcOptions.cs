78081562e4386e11b24e41fe0c315a126eb32f3d
杜冠魁 <hello@dignite.com>
2025-03-23 08:13:35 +0900

    Update PureThemeMvcOptions.cs

--- modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/PureThemeMvcOptions.cs
+++ modules/pure-theme/src/Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure/PureThemeMvcOptions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Dignite.Abp.AspNetCore.Mvc.UI.Theme.Pure.Themes.Pure.Components.Toolbar.NavbarToggler;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Toolbars;
 using Volo.Abp.UI.Navigation;
 
@@ -9,11 +11,11 @@ public class PureThemeMvcOptions
     /// <summary>
     /// A selector that defines which menu items will be displayed at mobile layout.
     /// </summary>
-    public Func<IReadOnlyList<ApplicationMenuItem>, IEnumerable<ApplicationMenuItem>> MobileMenuSelector { get; set; }
+    public Func<IReadOnlyList<ApplicationMenuItem>, IEnumerable<ApplicationMenuItem>> MobileMenuSelector { get; set; } = (menuItems) => menuItems.Where(x => x.Items.IsNullOrEmpty());
 
 
     /// <summary>
     /// A selector that defines which toolbar items will be displayed at mobile layout.
     /// </summary>
-    public Func<IReadOnlyList<ToolbarItem>, IEnumerable<ToolbarItem>> MobileToolbarSelector { get; set; }
+    public Func<IReadOnlyList<ToolbarItem>, IEnumerable<ToolbarItem>> MobileToolbarSelector { get; set; } = (menuItems) => menuItems.Where(x => x.ComponentType == typeof(NavbarTogglerViewComponent));
 }

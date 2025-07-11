a40f584cd6ff63c6eb2448f02a710ab567c07731
杜冠魁 <hello@dignite.com>
2025-07-12 08:45:30 +0900

    Update CmsApplicationBuilderExtensions.cs

--- modules/cms/src/Dignite.Cms.Public.Web/Builder/CmsApplicationBuilderExtensions.cs
+++ modules/cms/src/Dignite.Cms.Public.Web/Builder/CmsApplicationBuilderExtensions.cs
@@ -14,27 +14,30 @@ namespace Dignite.Cms.Public.Web.Builder
         /// <param name="app"></param>
         /// <param name="ignoredEndpoints"></param>
         /// <returns></returns>
-        public static IApplicationBuilder UseCmsEndpoints(this IApplicationBuilder app, string[] ignoredEndpoints=null)
+        public static IApplicationBuilder UseCmsEndpoints(this IApplicationBuilder app, bool shouldRegisterDefaultRoute=true, string[] excludedRoutePrefixes = null)
         {
-            var integrateIgnoredEndpoints = new List<string> { "swagger/", "abp/", "account/", "libs/", ".well-known/", "error$" };
-            if (ignoredEndpoints != null)
+            var predefinedExcludedPrefixes = new List<string> { "swagger/", "abp/", "account/", "libs/", ".well-known/", "error$" };
+            if (excludedRoutePrefixes != null)
             {
-                integrateIgnoredEndpoints.AddRange(ignoredEndpoints);
+                predefinedExcludedPrefixes.AddRange(excludedRoutePrefixes);
             }
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: CmsEndpointNames.DefaultEndpointName,
-                    pattern: "/",
-                    defaults: new { controller = CmsController.ControllerName, action = nameof(CmsController.Default) });
+                if (shouldRegisterDefaultRoute)
+                {
+                    endpoints.MapControllerRoute(
+                        name: CmsEndpointNames.DefaultEndpointName,
+                        pattern: "/",
+                        defaults: new { controller = CmsController.ControllerName, action = nameof(CmsController.Default) });
+                }
                 endpoints.MapControllerRoute(
                     name: CmsEndpointNames.RegionalizationEntryEndpointName,
                     pattern: "{culture:" + RegionalizationRouteConstraint.ConstraintName + "}/{*path}",
                     defaults: new { controller = CmsController.ControllerName, action = nameof(CmsController.CultureEntry) });
                 endpoints.MapControllerRoute(
                     name: CmsEndpointNames.EntryEndpointName,
-                    pattern: "{*path:regex(^(?!" + integrateIgnoredEndpoints.JoinAsString("|") + ").*)}",
+                    pattern: "{*path:regex(^(?!" + predefinedExcludedPrefixes.JoinAsString("|") + ").*)}",
                     defaults: new { controller = CmsController.ControllerName, action = nameof(CmsController.Entry) });
             });
 

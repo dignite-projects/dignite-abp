ee766ba8f2e5617983d8217bc1b0a3a3b8de9fe3
杜冠魁 <hello@dignite.com>
2025-08-01 05:57:31 +0900

    Add migration for Post entity versioning
    Introduced new EF Core migration 'AddPostEntityVersion' to support versioning for the Post entity. Updated the model snapshot to reflect the new schema changes.

--- modules/publisher/app/Dignite.Publisher.Demo.EntityFrameworkCore/Migrations/20250731205027_AddPostEntityVersion.Designer.cs
+++ modules/publisher/app/Dignite.Publisher.Demo.EntityFrameworkCore/Migrations/20250731205027_AddPostEntityVersion.Designer.cs
@@ -4,6 +4,7 @@ using Dignite.Publisher.Demo.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Migrations
 {
     [DbContext(typeof(DemoDbContext))]
-    partial class DemoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250731205027_AddPostEntityVersion")]
+    partial class AddPostEntityVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ modules/publisher/app/Dignite.Publisher.Demo.EntityFrameworkCore/Migrations/20250731205027_AddPostEntityVersion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPostEntityVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EntityVersion",
+                table: "pbl_Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EntityVersion",
+                table: "pbl_Posts");
+        }
+    }
+}

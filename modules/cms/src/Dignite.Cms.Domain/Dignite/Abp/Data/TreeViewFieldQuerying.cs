61fcad59a3b8db5b9455c8741ea32adacf8fec6e
杜冠魁 <hello@dignite.com>
2025-11-14 11:22:43 +0900

    Create TreeViewFieldQuerying.cs

---
+++ modules/cms/src/Dignite.Cms.Domain/Dignite/Abp/Data/TreeViewFieldQuerying.cs
@@ -0,0 +1,23 @@
+﻿using Dignite.Abp.DynamicForms.TreeView;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Linq;
+using Dignite.Cms.Entries;
+
+namespace Dignite.Abp.Data;
+public class TreeViewFieldQuerying : FieldQueryingBase<TreeViewFormControl>
+{
+
+    public TreeViewFieldQuerying() : base()
+    {
+    }
+
+
+    public override IEnumerable<Entry> Query([NotNull] IEnumerable<Entry> source, [NotNull] QueryingByField customField)
+    {
+        var value = customField.Value.Split(',');
+        return source.Where(e => e.ExtraProperties.ContainsKey(customField.Name) &&
+            e.GetField<string[]>(customField.Name).Any(v => value.Contains(v))
+        );
+    }
+}
